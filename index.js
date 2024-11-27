9259fc74530d61480a43c9852e8cbbf1b4dba34c
sqoobe <sqoobe96@gmail.com>
2024-11-27 10:37:02 +0100

    display cards

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.querySelector("#message-el")
-let sumEl = document.querySelector("sum-el")
-
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
